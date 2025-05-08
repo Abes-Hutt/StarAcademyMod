119eea0317b8f2f0191a2e03c8f8667531c38622
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-05-08 15:46:00 -0400

    Add crash detection and recovery for modernfix

---
+++ common/src/main/java/abeshutt/staracademy/mixin/modernfix/MixinLRUMap.java
@@ -0,0 +1,31 @@
+package abeshutt.staracademy.mixin.modernfix;
+
+import abeshutt.staracademy.StarAcademyMod;
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Set;
+
+@Mixin(targets = { "org.embeddedt.modernfix.util.LRUMap" })
+public class MixinLRUMap extends Object2ObjectLinkedOpenHashMap<Object, Object> {
+
+    @Redirect(method = "dropEntriesToMeetSize", at = @At(value = "INVOKE", target = "Ljava/util/Set;contains(Ljava/lang/Object;)Z", remap = false), remap = false)
+    public boolean dropEntriesToMeetSize(Set set, Object object) {
+        try {
+            return set.contains(object);
+        } catch(Exception e) {
+            StarAcademyMod.LOGGER.error("CRASH DETECTED START ======================");
+            e.printStackTrace();
+
+            this.forEach((key, value) -> {
+                StarAcademyMod.LOGGER.error("Key <{}>, value <{}>", key, value);
+            });
+
+            StarAcademyMod.LOGGER.error("CRASH DETECTED END ========================");
+            return false;
+        }
+    }
+
+}
