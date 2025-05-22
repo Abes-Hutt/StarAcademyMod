5735a0113e2b0017ba00e82546340cc84818120b
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-05-22 15:35:52 -0400

    Clean up journey map compat, support wavimons and genomons

--- common/src/main/java/abeshutt/staracademy/mixin/journeymap/MixinEntityHelper.java
+++ common/src/main/java/abeshutt/staracademy/mixin/journeymap/MixinEntityHelper.java
@@ -1,6 +1,7 @@
 package abeshutt.staracademy.mixin.journeymap;
 
 import abeshutt.staracademy.StarAcademyMod;
+import abeshutt.staracademy.compat.journeymap.JourneyMapCompat;
 import journeymap.client.model.entity.EntityHelper;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,13 +15,15 @@ public class MixinEntityHelper {
 
     @Inject(method = "getIconData", at = @At(value = "RETURN"), remap = false)
     private static void getIconData(Entity entity, CallbackInfoReturnable<EntityHelper.IconData> ci) {
-        StarAcademyMod.LOGGER.warn(ci.getReturnValue());
+        if(JourneyMapCompat.shouldLogIconData()) {
+            StarAcademyMod.LOGGER.warn(ci.getReturnValue());
+        }
     }
 
     @Redirect(method = "getIconData", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z"), remap = false)
     private static boolean equals(String instance, Object object) {
         if("cobblemon".equals(object)) {
-            return "cobblemon".equals(instance) || "wavimons".equals(instance);
+            return "cobblemon".equals(instance) || JourneyMapCompat.isCobblemonLike(instance);
         }
 
         return instance.equals(object);
