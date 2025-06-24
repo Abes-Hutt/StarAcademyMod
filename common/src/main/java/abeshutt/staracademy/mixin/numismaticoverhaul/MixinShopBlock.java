e7f52dee45c6623147c993ccb60065abc5ffded7
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-06-24 17:01:04 -0400

    Maybe fix shop crash

--- common/src/main/java/abeshutt/staracademy/mixin/numismaticoverhaul/MixinShopBlock.java
+++ common/src/main/java/abeshutt/staracademy/mixin/numismaticoverhaul/MixinShopBlock.java
@@ -2,17 +2,12 @@ package abeshutt.staracademy.mixin.numismaticoverhaul;
 
 import com.glisco.numismaticoverhaul.block.ShopBlock;
 import com.glisco.numismaticoverhaul.block.ShopBlockEntity;
-import com.glisco.numismaticoverhaul.currency.CurrencyConverter;
-
-import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
-import net.minecraft.util.ItemScatterer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ShopBlock.class)
 public abstract class MixinShopBlock extends BlockWithEntity {
@@ -23,27 +18,13 @@ public abstract class MixinShopBlock extends BlockWithEntity {
         super(settings);
     }
 
-    /**
-     * @author StarAcademyMod
-     * @reason Prevent inexhaustible shops from dropping currency.
-     */
-    @Overwrite
-    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
-        if(state.getBlock() != newState.getBlock()) {
-            if(world.getBlockEntity(pos) instanceof ShopBlockEntity shop) {
-                shop.getMerchant().setCustomer(null);
-
-                if(!this.inexhaustible) {
-                    CurrencyConverter.getAsValidStacks(shop.getStoredCurrency()).forEach(stack -> {
-                        ItemScatterer.spawn(world, pos.getX(), pos.getY(), pos.getZ(), stack);
-                    });
-                }
-
-                ItemScatterer.spawn(world, pos, shop);
-            }
-
-            super.onStateReplaced(state, world, pos, newState, moved);
+    @Redirect(method = "onStateReplaced", at = @At(value = "INVOKE", target = "Lcom/glisco/numismaticoverhaul/block/ShopBlockEntity;getStoredCurrency()J", remap = false))
+    public long onStateReplaced(ShopBlockEntity shop) {
+        if(this.inexhaustible) {
+            return 0L;
         }
+
+        return shop.getStoredCurrency();
     }
 
 }
