2af7b3506f1e63a9a5215cc8d405c8d2cd3072e2
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-08-08 21:30:38 -0400

    Clamp gym level slider to max pokemon level

---
+++ common/src/main/java/abeshutt/staracademy/mixin/MixinDiscreteSliderComponent.java
@@ -0,0 +1,32 @@
+package abeshutt.staracademy.mixin;
+
+import com.cobblemon.mod.common.Cobblemon;
+import io.wispforest.owo.ui.component.SliderComponent;
+import io.wispforest.owo.ui.core.Sizing;
+import io.wispforest.owo.ui.inject.ComponentStub;
+import lol.gito.radgyms.client.gui.GymGuiIdentifiers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = { "io.wispforest.owo.ui.component.DiscreteSliderComponent" }, remap = false)
+public abstract class MixinDiscreteSliderComponent extends SliderComponent {
+
+    @Shadow protected double max;
+
+    protected MixinDiscreteSliderComponent(Sizing horizontalSizing) {
+        super(horizontalSizing);
+    }
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lio/wispforest/owo/ui/component/DiscreteSliderComponent;updateMessage()V", shift = At.Shift.BEFORE), remap = false)
+    private void init(Sizing horizontalSizing, double min, double max, CallbackInfo ci) {
+        if(this instanceof ComponentStub stub) {
+            if(GymGuiIdentifiers.ID_GYM_SLIDER.equals(stub.id())) {
+                this.max = Cobblemon.config.getMaxPokemonLevel();
+            }
+        }
+    }
+
+}
