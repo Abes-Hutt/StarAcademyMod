0a547d372768a6692d43a553826acf343c760680
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-08-27 21:15:40 -0400

    Make nurse npc slim

--- common/src/main/java/abeshutt/staracademy/init/ModRenderers.java
+++ common/src/main/java/abeshutt/staracademy/init/ModRenderers.java
@@ -62,7 +62,7 @@ public class ModRenderers extends ModRegistries {
                 EntityRenderers.register(ModEntities.SLINGSHOT.get(), FlyingItemEntityRenderer::new);
                 EntityRenderers.register(ModEntities.PARTNER_NPC.get(), ctx -> new HumanEntityRenderer<>(ctx, false, e -> e));
                 EntityRenderers.register(ModEntities.SAFARI_NPC.get(), ctx -> new HumanEntityRenderer<>(ctx, true, e -> e));
-                EntityRenderers.register(ModEntities.NURSE_NPC.get(), ctx -> new HumanEntityRenderer<>(ctx, false, e -> e));
+                EntityRenderers.register(ModEntities.NURSE_NPC.get(), ctx -> new HumanEntityRenderer<>(ctx, true, e -> e));
                 EntityRenderers.register(ModEntities.CARD_GRADER_NPC.get(), ctx -> new HumanEntityRenderer<>(ctx, false, e -> e));
                 EntityRenderers.register(ModEntities.SHOOTING_STAR.get(), ShootingStarRenderer::new);
             });
