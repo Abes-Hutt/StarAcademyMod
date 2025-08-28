fd0d68d30fad2188d7248ea8e60242b66d1505b4
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-08-27 21:03:08 -0400

    Make nurse heal your party on right click

--- common/src/main/java/abeshutt/staracademy/entity/NurseNPCEntity.java
+++ common/src/main/java/abeshutt/staracademy/entity/NurseNPCEntity.java
@@ -1,16 +1,22 @@
 package abeshutt.staracademy.entity;
 
 import abeshutt.staracademy.StarAcademyMod;
+import com.cobblemon.mod.common.Cobblemon;
+import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
+
+import static com.cobblemon.mod.common.util.LocalizationUtilsKt.commandLang;
 
 public class NurseNPCEntity extends HumanEntity {
 
@@ -47,7 +53,28 @@ public class NurseNPCEntity extends HumanEntity {
     }
 
     @Override
-    protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+    public void handleStatus(byte status) {
+        if(status == 14) {
+            for(int i = 0; i < 5; ++i) {
+                double d0 = this.random.nextGaussian() * 0.02;
+                double d1 = this.random.nextGaussian() * 0.02;
+                double d2 = this.random.nextGaussian() * 0.02;
+                this.getWorld().addParticle(ParticleTypes.HAPPY_VILLAGER, this.getParticleX(1.0), this.getRandomBodyY() + 1.0, this.getParticleZ(1.0), d0, d1, d2);
+            }
+        }
+
+        super.handleStatus(status);
+    }
+
+    @Override
+    protected ActionResult interactMob(PlayerEntity user, Hand hand) {
+        if(user instanceof ServerPlayerEntity player) {
+            PlayerPartyStore party = Cobblemon.INSTANCE.getStorage().getParty(player);
+            party.heal();
+            player.sendMessage(commandLang("healpokemon.heal", player.getName()), true);
+            this.getWorld().sendEntityStatus(this, (byte)14);
+        }
+
         return ActionResult.SUCCESS;
     }
 
