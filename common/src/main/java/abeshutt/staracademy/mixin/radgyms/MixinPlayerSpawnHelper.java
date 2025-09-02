f72768d2dc31b82956ebfc3746fe260bb70e5573
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-09-02 07:27:24 -0400

    Start rad gyms structure at (0, 0)

---
+++ common/src/main/java/abeshutt/staracademy/mixin/radgyms/MixinPlayerSpawnHelper.java
@@ -0,0 +1,31 @@
+package abeshutt.staracademy.mixin.radgyms;
+
+import lol.gito.radgyms.RadGyms;
+import lol.gito.radgyms.nbt.EntityDataSaver;
+import lol.gito.radgyms.nbt.GymsNbtData;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Random;
+
+@Mixin(targets = { "lol.gito.radgyms.world.PlayerSpawnHelper" })
+public class MixinPlayerSpawnHelper {
+
+    /**
+     * @author Academy
+     * @reason Start gym spawns at (0, 0).
+     */
+    @Overwrite
+    public final BlockPos getUniquePlayerCoords(ServerPlayerEntity serverPlayer, ServerWorld serverWorld) {
+        Random random = new Random(serverPlayer.getUuid().getMostSignificantBits());
+        int playerX = random.nextInt(200000);
+        int playerZ = GymsNbtData.INSTANCE.incrementVisitCount((EntityDataSaver)serverPlayer) * 128;
+        RadGyms.INSTANCE.debug("Derived player ${serverPlayer.name} unique X coordinate from UUID: $playerX");
+        RadGyms.INSTANCE.debug("Derived player ${serverPlayer.name} unique Z coordinate from UUID: ${border.boundWest.toLong() + playerZ}");
+        return new BlockPos(playerX, 0, playerZ);
+    }
+
+}
