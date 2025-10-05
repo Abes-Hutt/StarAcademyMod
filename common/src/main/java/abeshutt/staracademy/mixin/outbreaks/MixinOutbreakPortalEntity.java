aad2abd875b8e586c767329716cc2488109ffa6c
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-10-04 22:01:47 -0400

    Fix outbreaks rewards spawning when the portal despawns

---
+++ common/src/main/java/abeshutt/staracademy/mixin/outbreaks/MixinOutbreakPortalEntity.java
@@ -0,0 +1,27 @@
+package abeshutt.staracademy.mixin.outbreaks;
+
+import com.scouter.cobblemonoutbreaks.data.OutbreakWaveData;
+import com.scouter.cobblemonoutbreaks.portal.OutbreakPortal;
+import com.scouter.cobblemonoutbreaks.portal.entity.OutbreakPortalEntityTickData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = { "com.scouter.cobblemonoutbreaks.portal.entity.OutbreakPortalEntity" })
+public abstract class MixinOutbreakPortalEntity {
+
+    @Shadow public abstract OutbreakPortalEntityTickData getTickData();
+
+    @Shadow public abstract OutbreakPortal getPortal();
+
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/scouter/cobblemonoutbreaks/data/OutbreakWaveData;getWaves()I"))
+    public int tick(OutbreakWaveData instance) {
+        if (this.getTickData().getTicksActive() >= this.getPortal().getGateTimer()) {
+            return Integer.MAX_VALUE;
+        }
+
+        return instance.getWaves();
+    }
+
+}
