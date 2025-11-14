5e6224bf909b246b18ef697d1aa4a6e56cb4600f
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-11-14 14:45:33 -0500

    Make buffered image loading faster

--- common/src/main/java/abeshutt/staracademy/screen/overlay/SplashLoader.java
+++ common/src/main/java/abeshutt/staracademy/screen/overlay/SplashLoader.java
@@ -9,6 +9,7 @@ import net.minecraft.util.Identifier;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
@@ -45,14 +46,24 @@ public class SplashLoader {
         return Optional.empty();
     }
 
-    public static void load(MinecraftClient client, BufferedImage bufferedImage, Identifier id) {
+    public static void load(MinecraftClient client, BufferedImage bufferedImage, Identifier id, NativeImage.Format format) {
         int width = bufferedImage.getWidth();
         int height = bufferedImage.getHeight();
-        NativeImage image = new NativeImage(RGBA, width, height, false);
+        NativeImage image = new NativeImage(format, width, height, false);
+        int[] pixels;
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int argb = bufferedImage.getRGB(x, y);
+        if (bufferedImage.getRaster().getDataBuffer() instanceof DataBufferInt buffer) {
+            pixels = buffer.getData(); // Already ARGB
+        } else {
+            pixels = new int[width * height];
+            bufferedImage.getRGB(0, 0, width, height, pixels, 0, width);
+        }
+
+        int index = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++, index++) {
+                int argb = pixels[index];
                 int a = (argb >>> 24) & 0xFF;
                 int r = (argb >>> 16) & 0xFF;
                 int g = (argb >>> 8) & 0xFF;
@@ -67,22 +78,25 @@ public class SplashLoader {
     }
 
     public static void load(MinecraftClient client) {
+        long start = System.nanoTime();
+
         try {
             FileInputStream in = new FileInputStream(Path.of("config", StarAcademyMod.ID, "assets", "splash.gif").toFile());
 
             GifDecoder decoder = new GifDecoder();
             decoder.read(in);
+            StarAcademyMod.LOGGER.warn("Decoding splash frames took {} seconds.", (System.nanoTime() - start) / 1_000_000_000.0D);
 
             for (int i = 0; i < decoder.getFrameCount(); i++) {
                 BufferedImage bufferedImage = decoder.getFrame(i);
                 int delay = decoder.getDelay(i);
                 Identifier id = StarAcademyMod.id("splash/frame/" + i);
-                load(client, bufferedImage, id);
+                load(client, bufferedImage, id, RGBA);
                 FRAMES.put(id, delay / 50.0d);
             }
 
             load(client, ImageIO.read(Path.of("config", StarAcademyMod.ID, "assets", "logo.png").toFile()),
-                    StarAcademyMod.id("splash/logo"));
+                    StarAcademyMod.id("splash/logo"), RGBA);
         } catch (Exception e) {
             e.printStackTrace();
         }
