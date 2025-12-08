52d4ec75bcca1c6ea2df339c64c05fcc5a7681f1
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-12-07 22:39:43 -0500

    Add dimension filter for yeeter config

--- common/src/main/java/abeshutt/staracademy/config/EntityYeeterConfig.java
+++ common/src/main/java/abeshutt/staracademy/config/EntityYeeterConfig.java
@@ -3,10 +3,13 @@ package abeshutt.staracademy.config;
 import abeshutt.staracademy.data.entity.EntityPredicate;
 import com.google.gson.annotations.Expose;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.Identifier;
+
+import java.util.Map;
 
 public class EntityYeeterConfig extends FileConfig {
 
-    @Expose private EntityPredicate blacklist;
+    @Expose private Map<Identifier, EntityPredicate> blacklist;
 
     @Override
     public String getPath() {
@@ -14,7 +17,12 @@ public class EntityYeeterConfig extends FileConfig {
     }
 
     public boolean contains(Entity entity) {
-        return this.blacklist != null && this.blacklist.test(entity);
+        if (entity == null || entity.getWorld() == null) {
+            return false;
+        }
+
+        Identifier dimension = entity.getWorld().getRegistryKey().getValue();
+        return this.blacklist != null && this.blacklist.getOrDefault(dimension, EntityPredicate.FALSE).test(entity);
     }
 
     @Override
