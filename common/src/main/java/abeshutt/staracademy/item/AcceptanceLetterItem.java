de0908bbee0122ecb1d4667f2ff7ea10b2d388ee
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-12-15 06:46:19 -0500

    Default acceptance letter owner

--- common/src/main/java/abeshutt/staracademy/item/AcceptanceLetterItem.java
+++ common/src/main/java/abeshutt/staracademy/item/AcceptanceLetterItem.java
@@ -10,10 +10,12 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -125,4 +127,12 @@ public class AcceptanceLetterItem extends Item {
         return TypedActionResult.consume(stack);
     }
 
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+
+        if (entity instanceof ServerPlayerEntity player && stack.get(ACCEPTANCE_LETTER_OWNER.get()) == null) {
+            stack.set(ACCEPTANCE_LETTER_OWNER.get(), player.getUuid());
+        }
+    }
 }
